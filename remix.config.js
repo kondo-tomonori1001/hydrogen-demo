8672aaeee7f7a7a93a773fdc30fa68ca20c02822
kondo tomonori <tomonorik1001@gmail.com>
2023-02-27 14:49:27 +0900

    修正

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,7 @@ module.exports = {
   appDirectory: 'app',
   ignoredRouteFiles: ['**/.*'],
   watchPaths: ['./public'],
-  server: './_middleware.ts',
+  server: './api/_middleware.ts',
   /**
    * The following settings are required to deploy Hydrogen apps to Oxygen:
    */
